36863a206569438dc498cfd8bead80b27d01b9d7
CbollaBinaria <sebastian.ramos@binariaseguros.com.ar>
2022-04-21 12:05:28 -0300

    Agregado Widget e ItemList

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
         <NavBar/>
         <ItemListContainer title="Productos destacados"/>
+        <ItemListContainer title="Ofertas del mes"/>
     </div>
   );
 }
