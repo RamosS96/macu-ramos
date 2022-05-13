1de075e1ae93c4214bc6739e189ee02951d2b525
Sebastian Ramos <sebastianalejandroramos96@gmail.com>
2022-05-13 15:16:06 -0300

    Inicio Firebase

--- src/pages/ItemListContainer/ItemListContainer.jsx
+++ src/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,43 +1,34 @@
 import { useParams } from "react-router-dom";
 import ShopCard from "../../components/Cards/ShopCard";
-import { initializeApp } from "firebase/app";
 import { collection, query, getDocs, getFirestore } from 'firebase/firestore';
 import { useEffect, useState } from "react";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDBVA0d5eVRdLbiZ73c9rnVvdzxedAw7v4",
-    authDomain: "macu-app-sr.firebaseapp.com",
-    projectId: "macu-app-sr",
-    storageBucket: "macu-app-sr.appspot.com",
-    messagingSenderId: "1042740416483",
-    appId: "1:1042740416483:web:d848412c2b6da7b63a1ad6",
-    measurementId: "G-PXBC978P1D"
-  };
-  
-  initializeApp(firebaseConfig);
-const db = getFirestore();
+function getProducts(){
+    const db = getFirestore();
+    const productCollection = collection(db, 'items');
 
-async function getProducts(){
-    
-    const itemCollection = collection(db, 'items');
-    const itemSnapshot = await getDocs(itemCollection);
-    const itemList = itemSnapshot.docs.map(doc => {return {...doc.data(), id: doc.id}})
+    const q = query(
+        productCollection
+    );
 
-    return itemList;
-    
+    return getDocs(q);
 }
 
+
 function ItemListContainer({ title }) {
- const [productos, setProductos] = useState();
+ const [productos, setProductos] = useState([]);
  const {categoryID} = useParams();
 
 useEffect(()=>{ 
-    getProducts()
-    .then(res => setProductos(res));
-    
+    getProducts(categoryID)
+    .then(snapshot => {
+        setProductos(snapshot.docs.map(doc => {
+            return {...doc.data(), id: doc.id}
+        }));
+    })
 },[categoryID]
 )
-console.log(productos)
+
 
     return (
         <div>
